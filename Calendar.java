c6aab4f8a9efdc3b3cedc3a45a8298d4fd2a4b36
Gal <154674381+G-a-l-o-r@users.noreply.github.com>
2024-03-22 21:30:08 +0200

    done - The last time =)

--- Calendar.java
+++ Calendar.java
@@ -5,8 +5,8 @@ public class Calendar {
     // Starting the calendar on 1/1/1900
 	static int dayOfMonth = 1;   
 	static int month = 1;
-	static int year;
-	static int dayOfWeek = 6;     // 1.1.1900 was a Monday
+	static int year = 1900;
+	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
 	static int nDaysInMonth = 31; // Number of days in January
 	
 	/** 
@@ -17,12 +17,15 @@ public class Calendar {
 		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
 	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
 	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
-		 year = Integer.parseInt(args[0]);
+		int getYear = Integer.parseInt(args[0]);
 
 	    int debugDaysCounter = 0; 
 		int SundayFirstDay = 0;
 	    //// Write the necessary initialization code, and replace the condition
 	    //// of the while loop with the necessary condition 
+		while (year<getYear){
+	    	advance();
+	    }
 		while (year == Integer.parseInt(args[0])) {
 			if (dayOfWeek==1 && dayOfMonth==1) SundayFirstDay++;
 			if (dayOfWeek==1) {
@@ -32,7 +35,7 @@ public class Calendar {
 			debugDaysCounter++;
 			// if (debugDaysCounter == 5000) break;
 		}
-			System.out.println("During the 20th century, "+SundayFirstDay+" Sundays fell on the first day of the month");
+			// System.out.println("During the 20th century, "+SundayFirstDay+" Sundays fell on the first day of the month");
         }
 
 		
